d2734d8962df0dbf781d2a0e4fa07964f796f331
Orçun AŞIK <asikorcun@gmail.com>
2022-12-22 19:33:34 +0300

    An abstract class "BaseEntity" is defined for all Models(Entities).

---
+++ Traversal.Core/Models/Abstract/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traversal.Core.Models.Abstract
+{
+    public abstract class BaseEntity
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
+        public virtual DateTime UpdatedDate { get; set; } = DateTime.Now;
+        public virtual bool IsDeleted { get; set; } = false;
+        public virtual bool IsActive { get; set; } = true;
+        public virtual string CreatedByName { get; set; }
+        public virtual string UpdatedByName { get; set; }
+    }
+}
