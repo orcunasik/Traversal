062001ef70f8d6f89deea42bc8833db695c4e49e
Orçun AŞIK <asikorcun@gmail.com>
2022-12-28 19:52:26 +0300

    GenericService interface was created for all services.

---
+++ Traversal.Core/Services/IService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Traversal.Core.Models.Abstract;
+
+namespace Traversal.Core.Services
+{
+    public interface IService<T> where T : class, IEntity, new()
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+
+    }
+}
