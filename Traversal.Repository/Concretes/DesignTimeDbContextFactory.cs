80a8e30630a4e740b2f8d6485ba302ea89e66f9e
Orçun AŞIK <asikorcun@gmail.com>
2022-12-28 15:55:40 +0300

    We built our DesignTime class to avoid errors when we create a migration via Powershell.

---
+++ Traversal.Repository/Concretes/DesignTimeDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traversal.Repository.Concretes
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<TraversalContext>
+    {
+        public TraversalContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<TraversalContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            return new(dbContextOptionsBuilder.Options);
+        }
+    }
+}
