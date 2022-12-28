430665569ea647954b66a08ba4be48b651409600
Orçun AŞIK <asikorcun@gmail.com>
2022-12-28 15:44:39 +0300

    Created DbContext class

---
+++ Traversal.Repository/Concretes/TraversalContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Traversal.Core.Models.Concrete;
+
+namespace Traversal.Repository.Concretes
+{
+    public class TraversalContext : DbContext
+    {
+        public TraversalContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<About2> About2s { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+        public DbSet<Newsletter> Newsletters { get; set; }
+        public DbSet<SubAbout> SubAbouts { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
