038c6be0ac80664580be3e8f54edcb48a68d6d4b
Orçun AŞIK <asikorcun@gmail.com>
2022-12-28 19:57:02 +0300

    Added our class to implement the UnitOfWork interface.

---
+++ Traversal.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Traversal.Core.UnitOfWorks;
+using Traversal.Repository.Concretes;
+
+namespace Traversal.Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TraversalContext _context;
+
+        public UnitOfWork(TraversalContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
