0bcedb6b3fb2ad6aeeb9edfee62435c5fcadb0cc
Orçun AŞIK <asikorcun@gmail.com>
2022-12-28 17:08:02 +0300

    Abstractions required for GenericRepository structuring have been created.

---
+++ Traversal.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Traversal.Core.Models.Abstract;
+
+namespace Traversal.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class,IEntity, new()
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        IQueryable<T> GetAll();
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+    }
+}
---
+++ Traversal.Core/Repositories/ISubAboutRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Traversal.Core.Models.Concrete;
+
+namespace Traversal.Core.Repositories
+{
+    public interface ISubAboutRepository : IGenericRepository<SubAbout>
+    {
+    }
+}
