da92f5356fa6dc63450e254a4a663b978747690f
orcunasik <142536Orc>
2022-12-29 21:01:32 +0300

    Relevant references have been transferred to the DI Container side.

--- Traversal.Mvc/Program.cs
+++ Traversal.Mvc/Program.cs
@@ -1,11 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
+using Traversal.Core.Repositories;
+using Traversal.Core.Services;
+using Traversal.Core.UnitOfWorks;
 using Traversal.Repository.Concretes;
+using Traversal.Repository.Repositories;
+using Traversal.Repository.UnitOfWorks;
+using Traversal.Service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddTransient(typeof(IService<>), typeof(Service<>));
+builder.Services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IAboutService, AboutService>();
+builder.Services.AddScoped<IAboutRepository, AboutRepository>();
 builder.Services.AddDbContext<TraversalContext>(x =>
 {
     x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"),option =>
