549325f3eb0a8f7bb72a7ffd5d880023722a9d64
Orçun Aşık <asikorcun@gmail.com>
2023-01-17 19:40:37 +0300

    Created BaseDto for Dto classes.

---
+++ Traversal.Core/DTOs/Abstract/BaseDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traversal.Core.DTOs.Abstract
+{
+    public abstract class BaseDto
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public bool IsActive { get; set; } = true;
+        public string CreatedByName { get; set; }
+        public string UpdatedByName { get; set; }
+    }
+}
